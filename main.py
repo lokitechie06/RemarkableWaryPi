5cc24da2a058adae351aac6e35349eb61874babf
Lokesh <lokesh.adu@gmail.com>
2020-05-07 08:23:02 +0000

    practice sample problems

--- main.py
+++ main.py
@@ -20,16 +20,60 @@ that the array is length 3 or more.
 
 """
 
-def string_splosion(str):
-  print("str", str)
-  print("len of str", len(str))
-  xplosed = ''
-  for i in range(len(str)+1):
-    print(str[:i])
-    xplosed += str[:i]
-  return xplosed
+def odd_positions(input):
+  output = input[1::2]
+  return output
+
+assert odd_positions([0,1,2,3,4,5,6]) == [1,3,5]
+assert odd_positions([0,-1,2,-2])==[-1,-2]
+
+print(odd_positions([0,1,2,3,4,5,6]))
+print(odd_positions([0,-1,2,-2]))
 
 
-print(string_splosion('Code')) # CCoCodCode
-print(string_splosion('abc')) # aababc
-print(string_splosion('ab')) # aab
+
+def cumm_sum_list(input):
+  output = []
+  sum = 0
+  for i in input:
+    sum += i
+    output.append(sum)
+  return output
+
+
+assert cumm_sum_list([1,1,1]) == [1,2,3]
+assert cumm_sum_list([1,-1,3]) == [1,0,3]
+
+print(cumm_sum_list([1,1,1]))
+print(cumm_sum_list([1,-1,3]))
+
+
+
+def output_list_digits(input):
+  output = []
+  inputStr = str(input)
+  for ch in inputStr:
+    output.append(int(ch))
+  return output
+
+assert output_list_digits(100) == [1,0,0]
+assert output_list_digits(400) == [4,0,0]
+
+print(output_list_digits(100))
+print(output_list_digits(400))
+
+
+
+def centered_avg_arr_of_ints(input):
+  return round((sum(input)-max(input)-min(input))/(len(input)-2));
+
+
+assert centered_avg_arr_of_ints([1, 2, 3, 4, 100]) == 3
+assert centered_avg_arr_of_ints([1, 1, 5, 5, 10, 8, 7]) == 5
+assert centered_avg_arr_of_ints([-10, -4, -2, -4, -2, 0]) == -3
+
+
+
+print(centered_avg_arr_of_ints([1, 2, 3, 4, 100]))
+print(centered_avg_arr_of_ints([1, 1, 5, 5, 10, 8, 7]))
+print(centered_avg_arr_of_ints([-10, -4, -2, -4, -2, 0]))
